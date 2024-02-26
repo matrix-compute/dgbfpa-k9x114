7747c7ce25571cd0871af19340545bfc34734806
matrix-compute <148979702+matrix-compute@users.noreply.github.com>
2024-02-26 13:31:02 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# dgbfpa-k9x114
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/matrix-compute/dgbfpa-k9x114)
---
+++ src/example/expansion-overview-example.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
+
+/**
+ * @title Basic expansion panel
+ */
+@Component({
+  selector: 'expansion-overview-example',
+  templateUrl: 'expansion-overview-example.html',
+  styleUrls: ['expansion-overview-example.css'],
+  standalone: true,
+  imports: [CommonModule, MatButtonModule, MatExpansionModule],
+})
+export class ExpansionOverviewExample {
+  panelOpenState = false;
+}
+
+/**  Copyright 2023 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at https://angular.io/license */
